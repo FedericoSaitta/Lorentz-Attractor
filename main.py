e90a2445b55d2b0cb93a82383031ecff9bb9f401
FedericoSaitta <144010367+FedericoSaitta@users.noreply.github.com>
2023-11-05 22:23:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import  FuncAnimation
-from scipy.integrate import odeint
 from Point_Class import Point
+from Animation_Class import Animation
 import timeit
-import random
 
 
+x_pos = (np.pi /2 * np.random.randn(50))
+y_pos = 0.5 * np.random.randn(50)
 
-
-obj_list = []
-
-x_pos = 50 * np.random.randn(50) - 25
-y_pos = 50 * np.random.randn(50) - 25
-z_pos = 50 * np.random.randn(50)
 
 for i in range(50):
-    Point((x_pos[i], y_pos[i], z_pos[i])).lorentz_data()
+    S_0 = (x_pos[i], y_pos[i])
+    Point(S_0, np.linspace(0,30, 600)).pendulum_data()
 
+a = Animation(Point.Pendulum_objs, np.linspace(0, 30, 600))
 
-
-'''x = Point((5,5,5))
-s = Point((5.1,5.1,5.1))
-z = Point((5.05,5.05,5.05))
-
-x.lorentz_data()
-s.lorentz_data()
-z.lorentz_data()'''
 
 start = timeit.default_timer()
 
-Point.make_animation()
+
+print(Point.Pendulum_objs)
+a.make_pendulum_animation()
+
+
+
 
 print("The difference of time is :", timeit.default_timer() - start)
